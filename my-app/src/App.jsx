47f36524bfded0d1024fd913f35615dd63ddc00c
vamsi99bhasaka <svk6466@gmail.com>
2025-09-02 18:55:53 +0530

    Worked on Forms Functionality

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Pages/Home'
+import EnquiryOptimized from './Pages/Enquiry/EnquiryOptimized'
 
 function App() {
 
   return (
     <>
-      
-      <Home />
+      {/* <Home /> */}
+      {/* <Enquiry />
+      <EnquiryUncontrolled /> */}
+      {/* <EnquiryControlled /> */}
+      <EnquiryOptimized />
     </>
   )
 }
